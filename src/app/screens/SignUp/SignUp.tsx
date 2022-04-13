26ed9095506e3ec5bba40b24425028a378ef967e
frapybalazswelker <balazs.welker@frapy.co>
2022-04-13 23:36:46 +0200

    [#38] Create Sign Up page

--- src/app/screens/SignUp/SignUp.tsx
+++ src/app/screens/SignUp/SignUp.tsx
@@ -16,6 +16,8 @@ type Props = {};
 function SignUp({}: Props) {
   const navigate = useNavigate();
 
+  const success: boolean = false;
+
   return (
     <Stack
       alignItem="center"
@@ -27,44 +29,88 @@ function SignUp({}: Props) {
         <Logo application="frapy" type="full" size={42} />
       </Stack>
       <Card width={360}>
-        <Stack padding={[24, 32]}>
-          <Stack margin={[16, 0]} alignItem="center">
-            <Typography type="h5">Create new account</Typography>
-          </Stack>
-          <Stack rowGap={16} alignItem="center">
-            <Input type="text" labelText="Email" fullWidth />
-            <Input type="text" labelText="Username" fullWidth />
-            <Input type="text" labelText="Password" fullWidth />
-            <Input type="text" labelText="Confirm password" fullWidth />
-            <span>
-              <input type="checkbox" />
-              <label>Accept</label>
-            </span>
-            <Button fullWidth>Create Account</Button>
-            <Typography type="subhead4">OR</Typography>
-            <Button
-              color="secondary"
-              kind="tertiary"
-              renderIcon={
+        <>
+          {!success && (
+            <Stack padding={[24, 32]}>
+              <Stack margin={[16, 0]} alignItem="center">
+                <Typography type="h5">Create new account</Typography>
+              </Stack>
+              <Stack rowGap={16} alignItem="center">
+                <Input type="text" labelText="Email" fullWidth />
+                <Input type="text" labelText="Username" fullWidth />
+                <Input type="text" labelText="Password" fullWidth />
+                <Input type="text" labelText="Confirm password" fullWidth />
+                <span>
+                  <input type="checkbox" />
+                  <label>Accept</label>
+                </span>
+                <Button fullWidth>Create Account</Button>
+                <Typography type="subhead4">OR</Typography>
+                <Button
+                  color="secondary"
+                  kind="tertiary"
+                  renderIcon={
+                    <img
+                      src="https://static.frapy.co/assets/social-media/google-logo.svg"
+                      height={16}
+                    />
+                  }
+                  fullWidth
+                >
+                  Continue with Google
+                </Button>
+              </Stack>
+              <Stack margin={[16, 0]}>
+                <Divider />
+              </Stack>
+              <Stack
+                margin={[8, 0, 0, 0]}
+                direction="row"
+                justifyContent="center"
+              >
+                <Button kind="link" onClick={() => navigate("/")}>
+                  Sign in instead?
+                </Button>
+              </Stack>
+            </Stack>
+          )}
+          {success && (
+            <Stack padding={[24, 32]}>
+              <Stack margin={[16, 0]} alignItem="center">
+                <Typography type="h5">Verification email sent</Typography>
+              </Stack>
+              <Stack margin={[0, 0, 16, 0]} alignItem="center">
                 <img
-                  src="https://static.frapy.co/assets/social-media/google-logo.svg"
-                  height={16}
+                  src="https://static.frapy.co/assets/illustration/sent-message-animate.svg"
+                  height="350"
                 />
-              }
-              fullWidth
-            >
-              Continue with Google
-            </Button>
-          </Stack>
-          <Stack margin={[16, 0]}>
-            <Divider />
-          </Stack>
-          <Stack margin={[8, 0, 0, 0]} direction="row" justifyContent="center">
-            <Button kind="link" onClick={() => navigate("/")}>
-              Sign in instead?
-            </Button>
-          </Stack>
-        </Stack>
+              </Stack>
+              <Stack rowGap={16} alignItem="center">
+                <Typography type="body4" textAlign="center">
+                  Yaayy! Thanks for registering an account with Frapy. You are
+                  the coolest person in all the world!
+                </Typography>
+                <Typography type="body4" textAlign="center">
+                  We've sent an email to you to verify your email address and
+                  activate your account. The link in the email will expire in 24
+                  hours.
+                </Typography>
+              </Stack>
+              <Stack margin={[16, 0]}>
+                <Divider />
+              </Stack>
+              <Stack
+                margin={[8, 0, 0, 0]}
+                direction="column"
+                alignItem="center"
+              >
+                <Button kind="link" onClick={() => navigate("/")}>
+                  Resend verification Email
+                </Button>
+              </Stack>
+            </Stack>
+          )}
+        </>
       </Card>
     </Stack>
   );
